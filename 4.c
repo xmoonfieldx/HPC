d1ed51ebfc1def03d9202ac2fa9bfa2b41a3aa65
Manjeera Reddy <61614013+xmoonfieldx@users.noreply.github.com>
2022-12-29 21:59:01 +0530

    Update 4.c

--- 4.c
+++ 4.c
@@ -5,38 +5,24 @@
 int main()
 {
 	srand(time(0));
-	int n = rand() % 30;
-	printf("n: %d\n", n);
-	int a[100], i;
-	a[0] = 1, a[1]=1;
+	int i, n=rand()%40;
 	omp_set_num_threads(2);
+	int a[100] = { 1,1 };
+	printf("n:%d\n",n);
 #pragma omp parallel
 	{
 		if (omp_get_thread_num() == 0)
 		{
+			printf("Using thread 0\n");
+			printf("Computing series...\n\n");
 			for (i = 2; i < n; i++)
-				a[i] = a[i - 2] + a[i - 1];
+				a[i] = a[i - 1] + a[i - 2];
 		}
-		if(omp_get_thread_num()==1)
+		if (omp_get_thread_num() == 1)
 		{
+			printf("Using thread 1\n");
 			for (i = 0; i < n; i++)
 				printf("%d ", a[i]);
 		}
 	}
-/*#pragma omp parallel
-	{
-		if (omp_get_thread_num() == 0)
-		{
-			for (i = 2; i < n; i++)
-			{
-				a[i] = a[i - 1] + a[i - 2];
-			}
-		}
-		if (omp_get_thread_num() == 1)
-		{
-			for (i = 0; i < n; i++)
-				printf("%d ", a[i]);
-		}
-	}*/
-
 }
