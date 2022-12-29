956fe5cadb7b3bf807dd1b21cc094792d3471bd0
Manjeera Reddy <61614013+xmoonfieldx@users.noreply.github.com>
2022-12-29 18:38:05 +0530

    Update 9.c

--- 9.c
+++ 9.c
@@ -2,17 +2,16 @@
 #include<stdlib.h>
 #include<time.h>
 #include<mpi.h>
-#include <omp.h>
 int main(int argc, char* argv[])
 {
-	int numtasks, rc, sz, prev, next, rank, count, inmsg;
+	int numtasks, rank, rc, count, next, prev, sz, inmsg;
 	MPI_Status Stat;
-	double start; 
+	time_t st, et;
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+	sz = (numtasks / 2) * 2;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	sz = numtasks;
-	start = omp_get_wtime();
+	st = clock();
 	if (rank == 0) prev = sz - 1;
 	else prev = rank - 1;
 	if (rank == sz - 1) next = 0;
@@ -26,23 +25,20 @@ int main(int argc, char* argv[])
 		rc = MPI_Send(&rank, 1, MPI_INT, next, 1, MPI_COMM_WORLD);
 	}
 	MPI_Barrier(MPI_COMM_WORLD);
-	if (rank == 0) printf("Time taken by Blocking send/receive : %lf\n", omp_get_wtime() - start);
+	et = clock();
+	if (rank == 0) printf("Time taken by Blocking send/receive : %lf\n", (double)(et - st) / CLOCKS_PER_SEC);
 	MPI_Barrier(MPI_COMM_WORLD);
 	MPI_Request reqs[2];
 	MPI_Status stats[2];
-	start = omp_get_wtime();
-	sz = numtasks;
-	if (rank == sz - 1) next = 0;
+	st = clock();
+	if (rank == numtasks - 1) next = 0;
 	else next = rank + 1;
-	if (rank == 0) prev = sz - 1;
+	if (rank == 0) prev = numtasks - 1;
 	else prev = rank - 1;
 	MPI_Irecv(&inmsg, 1, MPI_INT, prev, 0, MPI_COMM_WORLD, &reqs[0]);
 	MPI_Isend(&rank, 1, MPI_INT, next, 0, MPI_COMM_WORLD, &reqs[1]);
 	MPI_Barrier(MPI_COMM_WORLD);
-	if (rank == 0) printf("Time taken by NonBlocking send/receive : %lf\n", omp_get_wtime() - start);
+	et = clock();
+	if (rank == 0) printf("Time taken by NonBlocking send/receive : %lf\n", (double)(et - st) / CLOCKS_PER_SEC);
 	MPI_Finalize();
-	
-	/*
-
-	*/
 }
